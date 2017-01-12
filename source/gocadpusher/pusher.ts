48b27bb4733987c7bf79564d1400b44ce3acf8a4
Larry Hill <tomella1@gmail.com>
2017-01-13 00:02:05 +1030

    Pusher now extends eventdispatcher

--- source/gocadpusher/pusher.ts
+++ source/gocadpusher/pusher.ts
@@ -1,9 +1,12 @@
-export abstract class Pusher<T> {
+import { EventDispatcher } from "../util/eventdispatcher";
+
+export abstract class Pusher<T> extends EventDispatcher {
    complete: boolean;
    abstract push(line: string): boolean;
    abstract get obj(): T;
 
    constructor() {
+      super();
       this.complete = false;
    }
 }
