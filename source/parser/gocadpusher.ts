4962b1cf1fd82179f665c7be829ae65192685ad2
Larry Hill <tomella1@gmail.com>
2017-01-13 00:10:30 +1030

    Convert to smaller events

--- source/parser/gocadpusher.ts
+++ source/parser/gocadpusher.ts
@@ -1,16 +1,35 @@
 import { Parser } from "./parser";
+import { PipeToThreedObj } from "../push3js/pipetothreedobj";
+
 
 export class GocadPusherParser extends Parser {
    static WORKER_NAME = "gocadpusher.js";
    private activeCount: number;
 
+   constructor(public options: any = {}) {
+      super();
+   }
+
    public parse(data: any): Promise<any> {
-      return new Promise<any>(resolve => {
-         let worker = new Worker(this.getBase() + GocadPusherParser.WORKER_NAME);
-         worker.addEventListener("message", function(response) {
-            console.log("worker gocadpusher.js finished" );
+      return new Promise<any>((resolve, reject) => {
+         let worker = new Worker(this.getWorkersBase() + GocadPusherParser.WORKER_NAME);
+         let pipe = new PipeToThreedObj();
+
+         pipe.addEventListener("complete", (event) => {
             worker.terminate();
-            resolve(response.data);
+            resolve(event.data);
+         });
+
+         pipe.addEventListener("error", (event) => {
+            worker.terminate();
+            console.log("There is an error with your pipes!");
+            reject(event.data);
+         });
+
+         worker.addEventListener("message", function(response) {
+            let message = JSON.parse(response.data);
+            pipe.pipe(message);
+            console.log("EVENT = " + message.eventName );
          });
 
          worker.addEventListener("error", function(err) {
