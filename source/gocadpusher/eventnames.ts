aaf7f46eeba9aa2dd36b7e2cdbe56ee774a8d000
Larry Hill <tomella1@gmail.com>
2017-01-12 23:57:38 +1030

    Define event names of interest

---
+++ source/gocadpusher/eventnames.ts
@@ -0,0 +1,12 @@
+export class EventNames {
+   static names = [
+      "bstones",
+      "borders",
+      "complete",
+      "header",
+      "vertices",
+      "faces",
+      "lines",
+      "properties"
+   ];
+}
