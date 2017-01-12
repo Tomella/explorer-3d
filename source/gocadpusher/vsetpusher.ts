f920485cf7593e8d609a5c0d1be0410f8b244a8e
Larry Hill <tomella1@gmail.com>
2017-01-13 00:06:19 +1030

    Add incremental events to save memory

--- source/gocadpusher/vsetpusher.ts
+++ source/gocadpusher/vsetpusher.ts
@@ -1,5 +1,7 @@
 import { atom } from "../gocad/atom";
 import { vertex } from "../gocad/vertex";
+import { BoxFactory } from "../factory/boxfactory";
+import { Event } from "../domain/event";
 import { VSet } from "../gocad/vset";
 import { Pusher } from "./pusher";
 import { StatePusher } from "./statepusher";
@@ -7,14 +9,17 @@ import { VSetHeaderPusher } from "./vsetheaderpusher";
 import { CoordinateSystemPusher } from "./coordinatesystempusher";
 
 export class VSetPusher extends StatePusher<VSet> {
+   static PAGE_SIZE = 64 * 1024;
    vset: VSet;
    child: Pusher<any>;
    zSign: number;
+   verticesBuffer: any[];
    /**
     * We come in here on the next line
     */
    constructor(public projectionFn?: Function) {
       super();
+      this.verticesBuffer = [];
       this.vset = new VSet();
    }
 
@@ -59,6 +64,7 @@ export class VSetPusher extends StatePusher<VSet> {
          this.vset.header = this.child.obj;
          this.child = new CoordinateSystemPusher();
          this.zSign = 1;
+         this.dispatchEvent(new Event("header", this.vset));
       }
       return accepted;
    }
@@ -78,14 +84,39 @@ export class VSetPusher extends StatePusher<VSet> {
       if (line) {
          if (line.indexOf("VRTX") === 0 || line.indexOf("PVRTX") === 0) {
             let v = vertex(line, this.projectionFn, this.zSign);
-            this.vset.vertices[v.index] = v.all;
+            this.vset.bbox = BoxFactory.expand(this.vset.bbox, v.all);
+            this.checkVertices(v.all);
          } else if (line.indexOf("ATOM") === 0 || line.indexOf("PATOM") === 0) {
             let a = atom(line);
-            this.vset.vertices[a.index] = this.vset.vertices[a.vertexId];
+            this.checkVertices(this.vset.vertices[a.vertexId]);
          } else if (line.startsWith("END") && !line.startsWith("END_")) {
             this.complete = true;
+            this.flushVertices();
+            this.dispatchEvent(new Event("complete", {
+               header: this.vset.header,
+               coordinateSystem: this.vset.coordinateSystem,
+               bbox: this.vset.bbox
+            }));
          }
       }
       return true;
+   }
+
+   private checkVertices(vertex: any) {
+      this.verticesBuffer.push(vertex);
+      if (this.verticesBuffer.length >= VSetPusher.PAGE_SIZE) {
+         this.flushVertices();
+      }
+   }
+
+   private flushVertices() {
+      this.verticesBuffer = this.flush("vertices", this.verticesBuffer);
+   }
+
+   private flush(name: string, arr: any[]): any[] {
+      if (arr.length) {
+         this.dispatchEvent(new Event(name, arr));
+      }
+      return [];
    }
 }
