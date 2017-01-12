11b652b6c5729ac6f250e8ae38741565e67b6bf3
Larry Hill <tomella1@gmail.com>
2017-01-12 23:44:37 +1030

    Remove old style code

--- source/explorer3d.ts
+++ source/explorer3d.ts
@@ -2,20 +2,12 @@ export * from "./libs";
 export { loadBorders } from "./bridge3js/borders";
 export { loadBstones } from "./bridge3js/bstone";
 
-export { loadPLine } from "./bridge3js/pline";
-export { loadTSurf } from "./bridge3js/tsurf";
-export { loadVSet } from "./bridge3js/vset";
-export { Shaders } from "./shaders/shaders";
-
 export { DefaultWorldFactory } from "./view3d/defaultfactory";
 export { LabelSwitch } from "./modifiers/labelswitch";
 export { VerticalExagerate } from "./modifiers/verticalexagerate";
 
 export { FileDrop }     from "./filedrop/filedrop";
 export { Parser }       from "./parser/parser";
-export { Transformer }  from "./transformer/transform";
-export { GocadParser }  from "./parser/gocad";
 export { GocadPusherParser }  from "./parser/gocadpusher";
 export { LocalGocadPusherParser }  from "./parser/localgocadpusher";
 export { ThrottleProxyParser }  from "./parser/throttleproxyparser";
-export { GeoJsonParser } from "./parser/geojson";
