43d8cce4e7b19ad6a691313c6e29cc103178ab0b
Larry Hill <tomella1@gmail.com>
2017-01-12 23:46:52 +1030

    Remove old style code

--- source/libs.ts
+++ source/libs.ts
@@ -1,5 +1,6 @@
 // Rollup the libs as used by the web workers.
 // These are basically your domain objects, utilities, readers and transformers
-export { fileBufferedReader } from "./gocadbuffered/filebufferedreader";
-export { FilePusher } from "./gocadpusher/filepusher";
+export { DocumentPusher } from "./gocadpusher/documentpusher";
+export { LinesToLinePusher } from "./util/linestolinepusher";
+export { LinesPagedPusher } from "./util/linespagedpusher";
 
