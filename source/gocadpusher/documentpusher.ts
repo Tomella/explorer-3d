3629d6257a744ccca7b0e7ac4bd98d070d0cf8a0
Larry Hill <tomella1@gmail.com>
2017-01-12 23:57:10 +1030

    Make document pusher more pipeline like

--- source/gocadpusher/documentpusher.ts
+++ source/gocadpusher/documentpusher.ts
@@ -1,16 +1,30 @@
 import { Pusher } from "./pusher";
 import { TypeFactoryPusher } from "./typefactorypusher";
-import { Document } from "../gocad/document";
+import { GocadDocument } from "../gocad/gocaddocument";
+import { Event } from "../domain/event";
+import { EventNames } from "./eventnames";
 
-export class DocumentPusher extends Pusher<Document> {
-   document: Document;
+export class DocumentPusher extends Pusher<GocadDocument> {
+   document: GocadDocument;
    complete: boolean;
    typefactorypusher: TypeFactoryPusher;
+   projectionFn: Function;
 
-   constructor(public projectionFn?: Function) {
+   constructor(options: any, public proj4?: any) {
       super();
-      this.document = new Document();
-      this.typefactorypusher = new TypeFactoryPusher(this.projectionFn);
+      this.proj4.defs("EPSG:3112", "+proj=lcc +lat_1=-18 +lat_2=-36 +lat_0=0 +lon_0=134 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+
+      if (options && options.from && options.to && options.from !== options.to) {
+         this.projectionFn = function reproject(from, to) {
+            return function (coords) {
+               return proj4(from, to, [coords[0], coords[1]]);
+            };
+         }(options.from, options.to);
+      } else {
+         this.projectionFn = passThru;
+      }
+      this.document = new GocadDocument();
+      this.typefactorypusher = this.createTypeFactoryPusher(this.projectionFn);
    }
 
    get obj() {
@@ -30,8 +44,33 @@ export class DocumentPusher extends Pusher<Document> {
       if (this.typefactorypusher.complete) {
          this.complete = true;
          this.document.types.push(this.typefactorypusher.obj);
-         this.typefactorypusher = new TypeFactoryPusher(this.projectionFn);
+         this.destroyTypeFactoryPusher(this.typefactorypusher);
+         this.typefactorypusher = this.createTypeFactoryPusher(this.projectionFn);
       }
       return true;
    }
+
+   private createTypeFactoryPusher(projectionFn) {
+      let self = this;
+      let pusher = new TypeFactoryPusher(projectionFn);
+      EventNames.names.forEach(name => {
+         pusher.addEventListener(name, this.eventHandler);
+      });
+      return pusher;
+   }
+
+   private destroyTypeFactoryPusher(typefactorypusher: TypeFactoryPusher) {
+      EventNames.names.forEach(name => {
+         typefactorypusher.removeEventListener(name, this.eventHandler);
+      });
+   }
+
+   eventHandler = (event: Event) => {
+      // console.log("DP: " + event.type);
+      this.dispatchEvent(event);
+   }
+}
+
+function passThru(coords) {
+   return coords;
 }
