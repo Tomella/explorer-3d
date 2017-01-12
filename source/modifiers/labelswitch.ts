64642762e57fbc7722a40f1bf43e27832511cacd
Larry Hill <tomella1@gmail.com>
2017-01-13 00:06:45 +1030

    Simplify label switch

--- source/modifiers/labelswitch.ts
+++ source/modifiers/labelswitch.ts
@@ -1,19 +1,9 @@
 import { Modifier } from "./modifier";
 
 export class LabelSwitch extends Modifier {
-
    set (value) {
-      value ? this.on() : this.off();
-   }
-   on () {
       if (this.factory && this.factory.getWorld()) {
-         this.factory.getWorld().labels.visible = true;
-      }
-   }
-
-   off () {
-      if (this.factory && this.factory.getWorld()) {
-         this.factory.getWorld().labels.visible = false;
+         this.factory.getWorld().labels.visible = value;
       }
    }
 }
