3031fbc69517eb2ccbc38d2d8781672ed3833d77
Larry Hill <tomella1@gmail.com>
2017-01-17 23:02:31 +1030

    Add HttpPusher to libs

--- source/libs.ts
+++ source/libs.ts
@@ -2,6 +2,7 @@
 // These are basically your domain objects, utilities, readers and transformers
 export { DocumentPusher } from "./gocadpusher/documentpusher";
 export { LinesToLinePusher } from "./util/linestolinepusher";
+export { HttpPusher } from "./util/httppusher";
 export { LinesPagedPusher } from "./util/linespagedpusher";
 
 export { Logger } from "./util/logger";
