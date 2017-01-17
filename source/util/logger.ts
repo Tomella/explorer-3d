9f6301e6e60ff2296d978bc95f8354b2e6a14bea
Larry Hill <tomella1@gmail.com>
2017-01-17 18:35:48 +1030

    Refactor logger so it shows line numbers and file

--- source/util/logger.ts
+++ source/util/logger.ts
@@ -1,46 +1,41 @@
 export class Logger {
-   static _level: number = 0;
    static LOG_ALL = 64;
    static LOG_INFO = 32;
    static LOG_WARN = 16;
    static LOG_ERROR = 8;
    static LOG_NOTHING = 0;
+   private static _broken = false;
+
+   static noop() {};
+
+   static log: Function = Logger.noop;
+   static error: Function = Logger.noop;
+   static info: Function = Logger.noop;
+   static warn: Function = Logger.noop;
 
    static set level(value) {
       let num = parseInt(value);
       num = num === NaN ? 0 : num;
-      Logger._level = num;
-   }
-
-   static log(line, obj?) {
-      if (Logger._level >= Logger.LOG_ALL) {
-         Logger._log(line, obj);
+      if (!Logger._broken) {
+         Logger.log = console.log;
+         try {
+            Logger.log("Setting log level");
+         } catch (e) {
+            Logger._broken = true;
+         }
       }
-   }
 
-   static error(line, obj?) {
-      if (Logger._level >= Logger.LOG_ERROR) {
-         Logger._log("ERROR: " + line, obj);
-      }
-   }
-
-   static info(line, obj?) {
-      if (Logger._level >= Logger.LOG_INFO) {
-         Logger._log("INFO: " + line, obj);
-      }
-   }
-
-   static warn(line, obj?) {
-      if (Logger._level >= Logger.LOG_WARN) {
-         Logger._log("WARN: " + line, obj);
-      }
-   }
-
-   private static  _log(line, obj?) {
-      if (obj) {
-         console.log(line, obj);
+      if (Logger._broken) {
+         Logger.log = num >= 64 ? function() { console.log(...arguments); } : Logger.noop;
+         Logger.info = num >= 32 ? function() { console.info(...arguments); } : Logger.noop;
+         Logger.warn = num >= 16 ? function() { console.warn(...arguments); } : Logger.noop;
+         Logger.log = num >= 8 ? function() { console.log(...arguments); } : Logger.noop;
       } else {
-         console.log(line);
+         // We get to keep line numbers if we do it this way.
+         Logger.log = num >= 64 ? console.log : Logger.noop;
+         Logger.info = num >= 32 ? console.info : Logger.noop;
+         Logger.warn = num >= 16 ? console.warn : Logger.noop;
+         Logger.error = num >= 8 ? console.error : Logger.noop;
       }
    }
 }
